227a35a7457308a5953b334f91e871ecfdda3da4
charlene-johnson <charlene.j8234@gmail.com>
2020-07-15 22:51:27 -0400

    Fixed the title on the App.js file so that it is the right size and centered

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import PicOfDay from "./Components/PictureOfDay";
 import Navigation from "./Components/Navigation";
-import {ThemeProvider} from '@material-ui/core/styles';
-import {Typography} from '@material-ui/core'
-import theme from '../src/ui/Theme';
-import {BrowserRouter as Router} from 'react-router-dom'
-
-// const Title = styled.h2 `
-//   font-size: 5rem;
-//   font-family: 'Orbitron', sans-serif;
-// `
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import theme from "../src/ui/Theme";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  const[date, setDate] = useState(new Date()) 
+  const [date, setDate] = useState(new Date());
 
   return (
-    <ThemeProvider theme ={theme}>
-    <Router>
-      <Navigation date={date}
-                  setDate={setDate}
-      />
-          <Typography>Astronomy Picture of the Day</Typography>
-        
-        <PicOfDay date={date}/>
-        
-      </Router>  
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navigation date={date} setDate={setDate} />
+        <Typography
+          variant="h1"
+          style={{ fontSize: "5rem", textAlign: "center" }}
+        >
+          Astronomy Picture of the Day
+        </Typography>
+        <PicOfDay date={date} />
+      </Router>
     </ThemeProvider>
   );
 }
